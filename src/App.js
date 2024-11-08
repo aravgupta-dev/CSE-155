5d6c722f0f2843ddd250f4cbb1b86dbfd2373c1f
aravgupta-dev <agupta59@ucmerced.edu>
2024-11-07 22:28:49 -0800

    added changed ui

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more react
         </a>
       </header>
     </div>
